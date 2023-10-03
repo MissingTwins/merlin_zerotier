e7ce6ede16d1890ba3a4c2457146146c5b2d0a54
MissingTwins <146804746+MissingTwins@users.noreply.github.com>
2023-10-03 14:55:16 +0900

    Create lan-route-table.sh
    This should be in /jffs/scripts/

---
+++ lan-route-table.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+source /jffs/scripts/mylib.sh
+
+# -----------------------------------------
+## Check route for external LANs
+#echo "zt lan route:"
+addRoute "192.168.6.0/24"   "10.9.8.3"
+#addRoute "192.168.7.0/24"   "10.9.8.4"
+#addRoute "192.168.8.0/24"   "10.9.8.5"
+
+# -----------------------------------------
+# route for this LAN
+#echo "zt iptables:" 
+baseZTRoute "10.9.8.0/24" 
+	
+# -----------------------------------------
+# Redirect TCP/UDP IPv6 11111 to IPv4 192.168.9.5:3389
+#psCHK "socat TCP6-LISTEN:11111,fork TCP4:192.168.9.5:3389"
+#psCHK "socat UDP6-RECVFROM:11111,fork UDP4-SENDTO:192.168.9.5:3389"
+
+# ip4/6tables for RDP
+#ip6tablesINS "INPUT -i ppp+ -p tcp --dport 11111 -j ACCEPT"
+#ip6tablesINS "INPUT -i ppp+ -p udp --dport 11111 -j ACCEPT"
+
+#ip6tablesINS "INPUT -i eth+ -p tcp --dport 11111 -j ACCEPT"
+#ip6tablesINS "INPUT -i eth+ -p udp --dport 11111 -j ACCEPT"
+
+# job's RDP
+#iptablesINS "PREROUTING -t nat -s 192.168.9.0/24,10.9.8.0/24 -p tcp --dport 13389 -j DNAT --to-destination 192.168.102.204:3389"
+
