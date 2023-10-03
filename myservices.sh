63eeffea84e2a28ed174092055a0015cbcd8890b
MissingTwins <146804746+MissingTwins@users.noreply.github.com>
2023-10-03 15:13:33 +0900

    Update myservices.sh

--- myservices.sh
+++ myservices.sh
@@ -6,5 +6,10 @@ logger -t "myservices.sh" -c "Enter" -p user.notice
 #----------------------------------
 cru a ZeroTierDaemon  "*/1 * * * * /opt/etc/init.d/S90zerotier-one.sh start"
 
+#----------------------------------
+# remove double quote in zerotier task. Bug?
+cru a cruGuard1 "*/2 * * * * /jffs/scripts/cru_guard.sh"
+cru a cruGuard2 "*/3 * * * * /jffs/scripts/cru_guard.sh"
+
 echo "after myservices.sh" 
 logger -t "myservices.sh" -c "Leaving" -p user.notice
