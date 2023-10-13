4d41670bdbdc5b023e60a21483000729387600b8
Chester Wood <chetstone@malamode.io>
2023-10-13 12:24:36 -0600

    Move scripts to subdirectories

--- scripts/lan-route-table.sh
+++ scripts/lan-route-table.sh
@@ -5,15 +5,14 @@ source /jffs/scripts/mylib.sh
 # -----------------------------------------
 ## Check route for external LANs
 #echo "zt lan route:"
-addRoute "192.168.6.0/24"   "10.9.8.3"
-#addRoute "192.168.7.0/24"   "10.9.8.4"
-#addRoute "192.168.8.0/24"   "10.9.8.5"
+#addRoute "172.24.0.0/24" "10.147.17.128"
+#addRoute "192.168.108.0/24"   "10.147.17.64"
 
 # -----------------------------------------
 # route for this LAN
-#echo "zt iptables:" 
-baseZTRoute "10.9.8.0/24" 
-	
+#echo "zt iptables:"
+baseZTRoute "10.147.17.0/24"
+
 # -----------------------------------------
 # Redirect TCP/UDP IPv6 11111 to IPv4 192.168.9.5:3389
 #psCHK "socat TCP6-LISTEN:11111,fork TCP4:192.168.9.5:3389"
@@ -28,4 +27,3 @@ baseZTRoute "10.9.8.0/24"
 
 # job's RDP
 #iptablesINS "PREROUTING -t nat -s 192.168.9.0/24,10.9.8.0/24 -p tcp --dport 13389 -j DNAT --to-destination 192.168.102.204:3389"
-
