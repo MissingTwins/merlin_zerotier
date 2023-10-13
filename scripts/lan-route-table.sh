d91888cf1f48803ce151dfe625cfffee673a9e1f
Chester Wood <chetstone@malamode.io>
2023-10-13 17:33:52 -0600

    New README
    (cherry picked from commit 27b818aaee3e1be6e0c4058bddedf9bb9724fcd0)

--- scripts/lan-route-table.sh
+++ scripts/lan-route-table.sh
@@ -5,8 +5,8 @@ source /jffs/scripts/mylib.sh
 # -----------------------------------------
 ## Check route for external LANs
 #echo "zt lan route:"
-#addRoute "172.24.0.0/24" "10.147.17.128"
-#addRoute "192.168.108.0/24"   "10.147.17.64"
+addRoute "172.24.0.0/24" "10.147.17.128"
+#addRoute "192.168.108.0/24" "10.147.17.64"
 
 # -----------------------------------------
 # route for this LAN
